fb7985ee859cde637572b1584567823721f40fc0
ColorOfCode <ColorOfCode@49de09a6-d112-11de-8184-f791cb96e2ed>
2009-12-03 06:12:52 +0000

    The Item delegates Lcn & FragmentCount to the FragmentList

--- MSDefragLib/Common/FragmentList.cs
+++ MSDefragLib/Common/FragmentList.cs
@@ -12,6 +12,47 @@ namespace MSDefragLib
             Fragments = new List<Fragment>();
         }
 
+        public UInt64 Lcn
+        {
+            get
+            {
+                Fragment fragment = Fragments.
+                    FirstOrDefault(x => x.Lcn == Fragment.VIRTUALFRAGMENT);
+                if (fragment == null)
+                    return 0;
+                return fragment.Lcn;
+            }
+        }
+
+        public int FragmentCount
+        {
+            get
+            {
+                int count = 0;
+
+                UInt64 Vcn = 0;
+                UInt64 NextLcn = 0;
+
+                foreach (Fragment fragment in Fragments)
+                {
+                    if (fragment.Lcn != Fragment.VIRTUALFRAGMENT)
+                    {
+                        if ((NextLcn != 0) && (fragment.Lcn != NextLcn))
+                            count++;
+
+                        NextLcn = fragment.Lcn + fragment.NextVcn - Vcn;
+                    }
+
+                    Vcn = fragment.NextVcn;
+                }
+
+                if (NextLcn != 0)
+                    count++;
+
+                return count;
+            }
+        }
+
         public IList<Fragment> Fragments
         { get; private set; }
     }
--- MSDefragLib/Common/ItemStruct.cs
+++ MSDefragLib/Common/ItemStruct.cs
@@ -19,11 +19,7 @@ namespace MSDefragLib
         {
             get
             {
-                Fragment fragment = FragmentList.Fragments.
-                    FirstOrDefault( x => x.Lcn == Fragment.VIRTUALFRAGMENT);
-                if (fragment == null)
-                    return 0;
-                return fragment.Lcn;
+                return FragmentList.Lcn;
             }
         }
 
@@ -36,28 +32,7 @@ namespace MSDefragLib
         {
             get
             {
-                int count = 0;
-
-                UInt64 Vcn = 0;
-                UInt64 NextLcn = 0;
-
-                foreach (Fragment fragment in FragmentList.Fragments)
-                {
-                    if (fragment.Lcn != Fragment.VIRTUALFRAGMENT)
-                    {
-                        if ((NextLcn != 0) && (fragment.Lcn != NextLcn))
-                            count++;
-
-                        NextLcn = fragment.Lcn + fragment.NextVcn - Vcn;
-                    }
-
-                    Vcn = fragment.NextVcn;
-                }
-
-                if (NextLcn != 0)
-                    count++;
-
-                return count;
+                return FragmentList.FragmentCount;
             }
         }
 
